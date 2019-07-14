766921557870c1f485fd12445df5fd0fdd48a213
Asmitech <corpneeraj@gmail.com>
2019-07-14 11:19:57 +0530

    Kitlabh Ji

--- Hello Asmi/src/asmi.java
+++ Hello Asmi/src/asmi.java
@@ -3,7 +3,7 @@ public class asmi {
 
 	public static void main(String args[]) {
 		// TODO Auto-generated method stub
-      System.out.println("Hello Asmi Baisa");
+      System.out.println("Hello Kitlabh Ji");
 	}
 
 }
